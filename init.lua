b88556803b0daa45f832db836f26ad20b6fc1cfc
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-09-25 22:18:42 +0200

    add static

--- init.lua
+++ init.lua
@@ -1,17 +1,25 @@
+local path = require('path')
 local Utopia = require('utopia')
 local favicon = require('favicon')
 local logger = require('logger')
+local static = require('static')
 
+local __dirname = module.dir
 local app = Utopia:new()
 local port = process.env.PORT or 8080
 
-app:use(favicon())
-app:use(logger())
+function customMiddleware (req, res, nxt)
+  if not string.find(req.url, '/static') then
+    res:finish('Hello from Utopia and Docker on Zeit!')
+  else
+    nxt()
+  end
+end
 
--- respond to all requests
-app:use(function (req, res)
-  res:finish('Hello from Utopia and Docker on Zeit!')
-end)
+app:use(logger('short'))
+app:use(favicon())
+app:use(customMiddleware)
+-- app:use(static(path.join(__dirname, 'public')))
 
 app:listen(port)
 print('Server started at localhost:' .. port)
--- package.lua
+++ package.lua
@@ -4,7 +4,8 @@ return {
   dependencies = {
     'voronianski/utopia',
     'voronianski/favicon',
-    'voronianski/logger'
+    'voronianski/logger',
+    'voronianski/static'
   },
   files = {
     '**.lua',
